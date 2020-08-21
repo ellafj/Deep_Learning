59f2af95573c713cbff032d7822c351373ff8e61
Ella Johnsen <47972861+ellafj@users.noreply.github.com>
2020-08-21 18:19:49 +0200

    jeg styrer

--- testfil.py
+++ testfil.py
@@ -7,16 +7,16 @@ import matplotlib.pyplot as plt
 from scipy.linalg import sqrtm
 #from skimage.transform import resize
 #import cv2
-#from keras.preprocessing.image import load_img, img_to_array, array_to_img
-#from keras.layers import Activation, Dense, Input, Dropout, BatchNormalization
-#from keras.layers import Conv2D, Conv2DTranspose, Input, Flatten, Dense, Reshape, Concatenate, Lambda, Layer
-#from keras.layers.merge import concatenate
-#from keras.models import Model
-#from keras.optimizers import Adam
-#from keras.losses import mean_squared_error, binary_crossentropy
-#from keras import metrics
-#from keras.applications.inception_v3 import preprocess_input, InceptionV3
-#import keras.backend as K
+from keras.preprocessing.image import load_img, img_to_array, array_to_img
+from keras.layers import Activation, Dense, Input, Dropout, BatchNormalization
+from keras.layers import Conv2D, Conv2DTranspose, Input, Flatten, Dense, Reshape, Concatenate, Lambda, Layer
+from keras.layers.merge import concatenate
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.losses import mean_squared_error, binary_crossentropy
+from keras import metrics
+from keras.applications.inception_v3 import preprocess_input, InceptionV3
+import keras.backend as K
 
 
 ## Constants
@@ -67,3 +67,117 @@ def initialize_training_set(): # N or batch size??
 
 training_set = initialize_training_set()
 print(training_set)
+
+class Sampling(Layer):
+  def call(self, inputs):
+    z_mean, z_log_var = inputs
+    batch = K.shape(z_mean)[0]
+    dim = K.shape(z_mean)[1]
+    epsilon = K.random_normal(shape=(batch, dim))
+    return z_mean + K.exp(0.5 * z_log_var) * epsilon
+"""
+#Taken directly from Keras documentation
+class Sampling(layers.Layer):
+#Uses (z_mean, z_log_var) to sample z, the vector encoding a digit.
+    def call(self, inputs):
+        z_mean, z_log_var = inputs
+        batch = tf.shape(z_mean)[0]
+        dim = tf.shape(z_mean)[1]
+        epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+        return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+"""
+
+"""
+def Encode_Decode(x):
+  #Encoding
+  x = Conv2D(filters = 32, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+  x = Conv2D(filters = 64, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+  x = Conv2D(filters = 128, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+  x = Conv2D(filters = 256, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+  x = Conv2D(filters = 512, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+  #Decoding
+  x_ = Conv2DTranspose(filters = 256, kernel_size = 3, strides = 2,  padding = 'same', activation = 'relu')(x)
+  x_ = Conv2DTranspose(filters = 128, kernel_size = 3, strides = 2,  padding = 'same', activation = 'relu')(x_)
+  x_ = Conv2DTranspose(filters = 64, kernel_size = 3, strides = 2,  padding = 'same', activation = 'relu')(x_)
+  x_ = Conv2DTranspose(filters = 32, kernel_size = 3, strides = 2,  padding = 'same', activation = 'relu')(x_)
+  x_ = Conv2DTranspose(filters = 3, kernel_size = 3, strides = 2,  padding = 'same', activation = 'sigmoid')(x_) 
+  return [x,x_] 
+"""
+"""
+def Encoding(x):
+  #Downsampling
+  x = Conv2D(filters = 32, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+  x = Conv2D(filters = 64, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+  x = Conv2D(filters = 128, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+  x = Conv2D(filters = 256, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+  return x
+
+def Decoding(x):
+  #Upsampling
+  x = Conv2DTranspose(filters = 128, kernel_size = 3, strides = 2,  padding = 'same', activation = 'relu')(x)
+  x = Conv2DTranspose(filters = 64, kernel_size = 3, strides = 2,  padding = 'same', activation = 'relu')(x)
+  x = Conv2DTranspose(filters = 32, kernel_size = 3, strides = 2,  padding = 'same', activation = 'relu')(x)
+  x = Conv2DTranspose(filters = 3, kernel_size = 3, strides = 2,  padding = 'same', activation = 'sigmoid')(x)
+  return x
+"""
+
+def VAE():
+  #Define encoder model.
+  input_img = Input(shape = NEW_IMAGE_DIMS, name='input_img')
+  labels = Input(shape = (NUM_ATTRIBUTES,), name='labels')
+
+  x = Conv2D(filters = 32, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(input_img)
+  x = Conv2D(filters = 64, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+  x = Conv2D(filters = 128, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+  x = Conv2D(filters = 256, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+  x = Conv2D(filters = 512, kernel_size = 3, strides = 2, padding = 'same', activation = 'relu')(x)
+
+  #encode = Encode_Decode(input_img) #Done
+
+  shape_before_flattening = K.int_shape(x)[1:]
+  #shape_before_flattening = K.int_shape(encode)[1:]
+
+  x = Flatten()(x)
+
+  z_mean = Dense(LATENT_DIM, name='z_mean')(x)
+  z_log_sigma = Dense(LATENT_DIM, name='z_log_sigma')(x)
+  z = Sampling()([z_mean, z_log_sigma])
+
+  zy = Concatenate()([z, labels])
+
+  inputs_embedding = Input(shape=(Latent_Dim + Attributes_length,))
+  embedding = Dense(np.prod(shape_before_flattening))(inputs_embedding)
+  embedding = Reshape(shape_before_flattening)(embedding)
+
+  #Decoding
+  x_ = Conv2DTranspose(filters = 256, kernel_size = 3, strides = 2,  padding = 'same', activation = 'relu')(embedding)
+  x_ = Conv2DTranspose(filters = 128, kernel_size = 3, strides = 2,  padding = 'same', activation = 'relu')(x_)
+  x_ = Conv2DTranspose(filters = 64, kernel_size = 3, strides = 2,  padding = 'same', activation = 'relu')(x_)
+  x_ = Conv2DTranspose(filters = 32, kernel_size = 3, strides = 2,  padding = 'same', activation = 'relu')(x_)
+  x_ = Conv2DTranspose(filters = 3, kernel_size = 3, strides = 2,  padding = 'same', activation = 'sigmoid')(x_)
+
+  #x_hat = Encode_Decode(embedding)
+
+  encoder = Model(inputs = [input_img, labels], outputs = zy, name="encoder")
+  decoder = Model(inputs = inputs_embedding, outputs = x_, name="decoder")
+
+  vae_out = decoder(encoder([input_img, labels]))
+
+  rec_loss =  np.prod(NEW_IMAGE_DIMS) * binary_crossentropy(Flatten()(input_img), Flatten()(vae_out))
+  kl_loss = - 0.5 * K.sum(1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma), axis=-1)
+  vae_loss = K.mean(rec_loss + kl_loss)
+
+  vae = Model(inputs = [input_img, labels], outputs = vae_out, name="vae")
+
+  vae.add_loss(vae_loss)
+
+  optimizer = Adam(lr=0.0005, beta_1 = 0.5)
+  vae.compile(optimizer)
+
+  return vae, encoder, decoder
+
+vae, encoder, decoder = VAE()
+
+encoder.summary()
+decoder.summary()
+
